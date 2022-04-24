673d5fe4fec626c44361adb078bd2a6332589861
RUPESH KUMAR UPADHYAY <upadhyay.rupesh@gmail.com>
2022-04-24 15:49:40 +0530

    Add files via upload
    Excel Data Processing Component

---
+++ slnExcelDataUpload/ExcelDataUpload.API/Controllers/ExcelDataProcessController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using ExcelDataReader;
+using ExcelDataUpload.API.Helper;
+using Microsoft.Extensions.Configuration;
+
+namespace ExcelDataUpload.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelDataProcessController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+       
+        private readonly DataHandling objHelperDataAccess;
+
+        public ExcelDataProcessController(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
+        {
+            _webHostEnvironment = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+            objHelperDataAccess = new DataHandling(configuration);
+        }
+
+        [Route("UploadExcelData")]
+        [HttpPost]
+        public IActionResult ProcessExcelFile(List<IFormFile> files)
+        {
+            try
+            {
+                if (files.Count == 0)
+                    return BadRequest();
+
+                string message = "";
+
+                DataSet dsexcelRecordsAll = new DataSet();
+
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+                foreach (var file in files)
+                {
+                    IFormFile Inputfile = null;
+                    Stream FileStream = null;
+                    IExcelDataReader reader = null;
+                    DataSet dsexcelRecords = new DataSet();
+
+                    Inputfile = file;
+                    
+                    FileStream = Inputfile.OpenReadStream();
+
+                    if (Inputfile != null && FileStream != null)
+                    {
+                        if (Inputfile.FileName.EndsWith(".xls"))
+                        { reader = ExcelReaderFactory.CreateBinaryReader(FileStream); }
+                        else if (Inputfile.FileName.EndsWith(".xlsx"))
+                        { reader = ExcelReaderFactory.CreateOpenXmlReader(FileStream); }
+                        else { message = "The file format is not supported."; }
+
+                        dsexcelRecords = reader.AsDataSet();
+                        reader.Close();
+
+                        if (dsexcelRecords != null && dsexcelRecords.Tables.Count > 0)
+                        {
+                            objHelperDataAccess.ProcessDataAdotoSQL(dsexcelRecords, "sampleTable1", null, false);
+                            //DataTable dtExcelRecords = dsexcelRecords.Tables[0];
+
+                            //Write Function to Convert copy data from bulk Copy.
+                            //dsexcelRecordsAll.Tables.Add(dsexcelRecords.Tables[0]);
+                        }
+                        else { message = "Selected file is empty."; }
+                    }
+                    else { message = "Invalid File."; }
+                }
+
+                return Ok(message);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ slnExcelDataUpload/ExcelDataUpload.API/Helper/DataHandling.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ExcelDataUpload.API.Helper
+{
+    public class DataHandling
+    {
+        private readonly string Dbconnectionstring; 
+
+        public readonly IConfiguration  _configuration;
+
+        public DataHandling(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            Dbconnectionstring = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");     
+        }
+
+        public bool ProcessDataAdotoSQL(DataSet srcDataSet, string dbDestinationTblNm,List<string> dbDestTableList, bool blncolumnMappingActionSts=true)
+        {
+            Boolean datatransferSts = false;
+            try
+            {
+                if(srcDataSet != null && srcDataSet.Tables.Count>0)
+                {
+                    foreach(var table in srcDataSet.Tables)
+                    {
+                        using(SqlConnection objSqlConn=new SqlConnection(Dbconnectionstring))
+                        {
+                            objSqlConn.Open();
+                            if (dbDestTableList !=null && dbDestTableList.Count>0)
+                            {
+                                //Write Code if any validation on Table Data Required or Data Require to delete first.
+                            }
+
+                            using (SqlBulkCopy objSqlBulkCopy = new SqlBulkCopy(objSqlConn))
+                            {
+                                objSqlBulkCopy.BatchSize = 100000;
+                                objSqlBulkCopy.BulkCopyTimeout = 1000;
+                                objSqlBulkCopy.DestinationTableName = dbDestinationTblNm;
+                                objSqlBulkCopy.ColumnMappings.Clear();
+
+                                if(blncolumnMappingActionSts)
+                                {
+                                    foreach (DataColumn objcolumn in srcDataSet.Tables[0].Columns)
+                                    {
+                                        objSqlBulkCopy.ColumnMappings.Add(objcolumn.ColumnName, objcolumn.ColumnName.ToUpper());
+                                    }
+                                }
+
+                                objSqlBulkCopy.WriteToServer(srcDataSet.Tables[0].CreateDataReader());
+                                datatransferSts = true;
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                datatransferSts = false;
+                throw;
+            }
+            finally
+            {
+                if (dbDestTableList != null)
+                { dbDestTableList.Clear();}
+
+
+                if (srcDataSet != null)
+                {
+                    srcDataSet=null;
+                }
+
+            }
+
+            return datatransferSts;
+;
+        }
+    }
+}
